45fe2678a67564ab98a69b94e4b106a695010347
Aftab Ahmed <ibaaftab12@gmail.com>
2023-12-02 14:52:06 +0500

    create basic template for react application

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ return (
             <Nav.Link className='text-light'><Link to={"/contact"} style={{textDecoration:"none",color:"white"}}>Contact Us</Link></Nav.Link>
             <Nav.Link className='text-light'><Link to={"/blog"} style={{textDecoration:"none",color:"white"}}>Blog</Link></Nav.Link>
             <Nav.Link className='text-light'><Link to={"/login"} style={{textDecoration:"none",color:"white" ,marginRight:"20rem"}}>Login</Link></Nav.Link>
-       <a target="_blank" class="btn text-white rounded-pill" style={{backgroundColor:"#43b665" }}type="primary" shape="round" href="https://drive.google.com/file/d/1GZZ2z_WwoLqknqbKIDVMeOk6rsu6_Uya/view?usp=sharing">
+       <a target="_blank" class="btn text-white rounded-pill" style={{backgroundColor:"#43b665" }}type="primary" shape="round" href="https://drive.google.com/file/d/1Wt3HNDEzNyAjmKE3MZCJ73SOzOcYiDLx/view?usp=drive_link">
         <span role="img" aria-label="download" class="anticon anticon-download"><svg viewBox="64 64 896 896" focusable="false" data-icon="download" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M505.7 661a8 8 0 0012.6 0l112-141.7c4.1-5.2.4-12.9-6.3-12.9h-74.1V168c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v338.3H400c-6.7 0-10.4 7.7-6.3 12.9l112 141.8zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"></path></svg></span> Download CV</a>
             
           </Nav>
